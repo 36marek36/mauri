05d301786057612ed8a1e29e3ad0548a0e91690b
marek krncok <krncok36@gmail.com>
2025-05-02 08:57:56 +0200

    Initial commit

---
+++ src/main/java/com/example/mauri/MauriApplication.java
@@ -0,0 +1,13 @@
+package com.example.mauri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mauri/MauriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mauri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
