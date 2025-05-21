c24c2f8549ddead73e164edf0df6df20b9782032
marek krncok <krncok36@gmail.com>
2025-05-21 08:18:34 +0200

    SeasonId in DTO, Add participants in league, remove participant from league,findPlayersNotInAnyLeague

--- src/main/java/com/example/mauri/repository/PlayerRepository.java
+++ src/main/java/com/example/mauri/repository/PlayerRepository.java
@@ -2,9 +2,14 @@ package com.example.mauri.repository;
 
 import com.example.mauri.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, String> {
+    @Query("SELECT p FROM players p WHERE p NOT IN (SELECT pl FROM leagues l JOIN l.players pl)")
+    List<Player> findPlayersNotInAnyLeague();
 
 }
