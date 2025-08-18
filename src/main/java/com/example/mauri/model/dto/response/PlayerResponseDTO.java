f3fbb96a7fe6aec7c6cb0bc9b16ab9dc748e0289
marek krncok <krncok36@gmail.com>
2025-08-18 20:30:37 +0200

    PlayerResponseDTO

---
+++ src/main/java/com/example/mauri/model/dto/response/PlayerResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.mauri.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlayerResponseDTO {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDate registrationDate;
+    private LocalDate deletedDate;
+    private boolean active;
+}
--- src/main/java/com/example/mauri/service/PlayerService.java
+++ src/main/java/com/example/mauri/service/PlayerService.java
@@ -2,28 +2,31 @@ package com.example.mauri.service;
 
 import com.example.mauri.model.Player;
 import com.example.mauri.model.dto.create.CreatePlayerDTO;
+import com.example.mauri.model.dto.response.PlayerResponseDTO;
 import com.example.mauri.model.dto.update.UpdatePlayerDTO;
 import lombok.NonNull;
 
 import java.util.List;
 
 public interface PlayerService {
-    List<Player> getActivePlayers();
+    List<PlayerResponseDTO> getActivePlayers();
 
-    List<Player> getInactivePlayers();
+    List<PlayerResponseDTO> getInactivePlayers();
 
     Player getPlayer(@NonNull String id);
 
-    Player createPlayer(CreatePlayerDTO createPlayerDTO);
+    PlayerResponseDTO getPlayerResponseById(String id);
+
+    PlayerResponseDTO createPlayer(CreatePlayerDTO createPlayerDTO);
 
     String deletePlayer(@NonNull String id);
 
-    List<Player> getActivePlayersNotInAnyActiveLeague();
+    List<PlayerResponseDTO> getActivePlayersNotInAnyActiveLeague();
 
-    List<Player> getPlayersWithoutUser();
+    List<PlayerResponseDTO> getPlayersWithoutUser();
 
     void deactivatePlayer(String playerId);
 
-    Player updatePlayer(String playerId, UpdatePlayerDTO updatedPlayer);
+    PlayerResponseDTO updatePlayer(String playerId, UpdatePlayerDTO updatedPlayer);
 
 }
