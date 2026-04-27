1695e9ec31c53b5058fd01129418474d5f1430cc
marek krncok <krncok36@gmail.com>
2026-04-27 23:00:49 +0200

    uprava pravidiel a vytvaranie timu

--- src/main/java/com/example/mauri/repository/TeamRepository.java
+++ src/main/java/com/example/mauri/repository/TeamRepository.java
@@ -19,10 +19,19 @@ public interface TeamRepository extends JpaRepository<Team, String> {
 
     List<Team> findByPlayer1IdOrPlayer2Id(String player1Id, String player2Id);
 
-    List<Team> findByActiveTrue();
+    List<Team> findByActiveTrueOrderByPlayer1LastNameAsc();
 
-    List<Team> findByActiveFalse();
+    List<Team> findByActiveFalseOrderByPlayer1LastNameAsc();
 
     @Query("SELECT t FROM teams t WHERE t.active = true and t.id NOT IN (SELECT tm.id FROM leagues l join l.teams tm WHERE l.id = :leagueId)")
     List<Team> findTeamsNotInLeague(@Param("leagueId") String leagueId);
+
+    @Query("""
+                SELECT COUNT(t) > 0 FROM teams t
+                WHERE
+                    (t.player1.id = :p1 AND t.player2.id = :p2)
+                    OR
+                    (t.player1.id = :p2 AND t.player2.id = :p1)
+            """)
+    boolean existsByPlayers(@Param("p1") String p1, @Param("p2") String p2);
 }
