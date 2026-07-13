e62d7c59ebc9ac0e3de63aa725af03c41d053e46
marek krncok <krncok36@gmail.com>
2026-07-13 14:50:22 +0200

    ReplaceTeamInLeague

---
+++ src/main/java/com/example/mauri/model/dto/update/ReplaceTeamDTO.java
@@ -0,0 +1,9 @@
+package com.example.mauri.model.dto.update;
+
+import lombok.Data;
+
+@Data
+public class ReplaceTeamDTO {
+    private String oldTeamId;
+    private String newTeamId;
+}
--- src/main/java/com/example/mauri/service/LeagueService.java
+++ src/main/java/com/example/mauri/service/LeagueService.java
@@ -3,6 +3,7 @@ package com.example.mauri.service;
 import com.example.mauri.model.League;
 import com.example.mauri.model.dto.create.CreateLeagueDTO;
 import com.example.mauri.model.dto.response.LeagueResponseDTO;
+import com.example.mauri.model.dto.update.ReplaceTeamDTO;
 import lombok.NonNull;
 
 import java.util.List;
@@ -17,4 +18,5 @@ public interface LeagueService {
     String dropParticipantFromLeague(String leagueId, String participantId);
     void finishLeague(String leagueId);
     LeagueResponseDTO getFullLeagueDTO(League league);
+    void replaceTeamInLeague(String leagueId, ReplaceTeamDTO replaceTeamDTO);
 }
